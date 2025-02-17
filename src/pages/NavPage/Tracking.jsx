37a0486119a2c0730d58f3972fe6fabbaa49a5d3
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-02-17 13:37:47 +0500

    trek page

--- src/pages/NavPage/Tracking.jsx
+++ src/pages/NavPage/Tracking.jsx
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '@/styles/main.scss'
 import { Link } from 'react-router-dom';
+import ReCAPTCHA from "react-google-recaptcha";
 
 function Tracking() {
-  
+  const [recaptchaValue, setRecaptchaValue] = useState(null);  
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -14,22 +24,27 @@ function Tracking() {
         <div className='tracking-Links'>
           <Link to='/'>Bosh sahifa</Link>
           <span>&gt;</span>
-          <p>To'lov</p>
+          <p>Buyurtma holati</p>
         </div>
         <div className='tracking-content'>
           <div className='tracking-content-registration'>
-            <h2>To'lov</h2>
-            <b>Buyurtma yoki shaxsiy ID raqamingizni bexato kiriting</b>
-            <input type="text" placeholder='Namuna: A777 yoki B999 yoki M333' maxLength={10} />
-            <div className='text'>
-              <p>B - buyurtma uchun</p>
-              <p>A - akkount uchun</p>
-              <p>M - rassrochkani so'ndirish uchun</p>
+            <h2>Buyurtma holati</h2>
+            <b>Buyurtmani ko'rish uchun kerakli maydonlarni to'ldiring. Buyurtma raqami sizning raqamingizga SMS-xabar shaklida yuborilgan</b>
+            <input type="text" placeholder='BUYURMA RAQAMI' />
+            <label htmlFor="tel">
+              <p>Buyurtma berilgandagi telefon raqami <span>*</span></p>
+              <input type="tel" name="" id="tel" />
+            </label>
+            <div className='capcha'>
+              <ReCAPTCHA
+                sitekey="6Le4qNkqAAAAADlnJGrpsNdMJpY5Q18947NNn86I" 
+                onChange={handleRecaptchaChange}
+              />
             </div>
-            <button className='tracking-btn'>Tasdiqlash</button>
+            <button className='tracking-btn' disabled={!recaptchaValue}>Kirish</button>
           </div>
           <div className='tracking-content-img'>
-            <img src="/assets/images/order-pay.svg" alt="" />
+            <img src="/assets/images/tracking.svg" alt="" />
           </div>
         </div>
       </div>
