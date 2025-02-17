21d96bb677335ea3cd22c311595aed409c0b6f17
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-02-17 13:11:06 +0500

    tolov page

--- src/pages/NavPage/Payment.jsx
+++ src/pages/NavPage/Payment.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '@/styles/main.scss'
+import { Link } from 'react-router-dom'
 
 function Payment() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <section className='payment'>
       <div className='container'>
-        <h1>Payment</h1>
+        <div className='payment-Links'>
+          <Link to='/'>Bosh sahifa</Link>
+          <span>&gt;</span>
+          <p>To'lov</p>
+        </div>
+        <div className='payment-content'>
+            <div className='payment-content-registration'>
+              <h2>To'lov</h2>
+              <b>Buyurtma yoki shaxsiy ID raqamingizni bexato kiriting</b>
+              <input type="text" placeholder='Namuna: A777 yoki B999 yoki M333' maxLength={10} />
+              <div className='text'>
+                <p>B - buyurtma uchun</p>
+                <p>A - akkount uchun</p>
+                <p>M - rassrochkani so'ndirish uchun</p>
+              </div>
+              <button className='payment-btn'>Tasdiqlash</button>
+            </div>
+            <div className='payment-content-img'>
+                <img src="/assets/images/order-pay.svg" alt="" />
+            </div>
+        </div>
       </div>
     </section>
   )
--- src/pages/NavPage/Tracking.jsx
+++ src/pages/NavPage/Tracking.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import '@/styles/main.scss' 
+import React, { useEffect } from 'react'
+import '@/styles/main.scss'
+import { Link } from 'react-router-dom';
 
 function Tracking() {
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <section className='tracking'>
       <div className='container'>
-        <h1>Tracking</h1>
+        <div className='tracking-Links'>
+          <Link to='/'>Bosh sahifa</Link>
+          <span>&gt;</span>
+          <p>To'lov</p>
+        </div>
+        <div className='tracking-content'>
+          <div className='tracking-content-registration'>
+            <h2>To'lov</h2>
+            <b>Buyurtma yoki shaxsiy ID raqamingizni bexato kiriting</b>
+            <input type="text" placeholder='Namuna: A777 yoki B999 yoki M333' maxLength={10} />
+            <div className='text'>
+              <p>B - buyurtma uchun</p>
+              <p>A - akkount uchun</p>
+              <p>M - rassrochkani so'ndirish uchun</p>
+            </div>
+            <button className='tracking-btn'>Tasdiqlash</button>
+          </div>
+          <div className='tracking-content-img'>
+            <img src="/assets/images/order-pay.svg" alt="" />
+          </div>
+        </div>
       </div>
     </section>
   )
