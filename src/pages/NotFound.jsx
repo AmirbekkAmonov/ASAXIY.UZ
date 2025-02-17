cf53e85868f45793b5459afaf621f30b4b41bbb6
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-02-17 14:40:41 +0500

    404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import '@/styles/main.scss'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div style={{marginTop: "70px"}}>NotFound</div>
+    <div className='notfound'>
+        <div className="container">
+         <div className="notfound-content">
+         <img src="/assets/images/img-error.svg" alt="" />
+            <h1>Sahifa topilmadi!</h1>
+            <p>Xavotirlanmang va xarid qilishda davom eting</p>
+            <Link to="/">Bosh sahifa</Link>
+         </div>
+        </div>
+    </div>
   )
 }
 
