8d336c587b1195f57cca89128daf4283cee2697b
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-02-17 14:27:08 +0500

    savat page

--- src/pages/NavPage/Basket.jsx
+++ src/pages/NavPage/Basket.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '@/styles/main.scss'
+import { Link } from 'react-router-dom'
 
 function Basket() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <section className='basket'>
       <div className='container'>
-        <h1>Basket</h1>
+        <div className='basket-Links'>
+          <Link to='/'>Bosh sahifa</Link>
+          <span>&gt;</span>
+          <p>Savatcha</p>
+        </div>
+        <div className="basket-empty">
+          <img src="/assets/images/basket_no_page.webp" alt="" />
+          <p>Savatda hozircha mahsulot yo'q.</p>
+          <span>Bosh sahifadagi termalardan boshlang yoki kerakli mahsulotni qidiruv orqali toping.</span>
+          <Link to='/' className="btn">Asosiy menyu</Link>
+        </div>
       </div>
     </section>
   )
